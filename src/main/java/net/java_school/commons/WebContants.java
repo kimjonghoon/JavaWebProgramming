e59948822580af504c4d4d892b3046674a841555
Jonghoon Kim <kim0070@gmail.com>
2024-02-28 11:15:34 +0900

    pom.xml updated

--- src/main/java/net/java_school/commons/WebContants.java
+++ src/main/java/net/java_school/commons/WebContants.java
@@ -10,6 +10,6 @@ public class WebContants {
     //Line Separator
     public final static String LINE_SEPARATOR = System.getProperty("line.separator");
     //Upload path
-    public final static String UPLOAD_PATH = "C:/Users/kim00/upload/";
+    public final static String UPLOAD_PATH = "/home/kim/upload/";
 
 }
